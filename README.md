66f9f3a84490bbc5059060e952a893c0eefa66ef
Samcyril21 <119171114+Samcyril21@users.noreply.github.com>
2023-02-27 13:14:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic-User-Authentication-Using-Javascript-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-b2qaj4)
---
+++ script.js
@@ -0,0 +1,44 @@
+let credentials=[
+  {
+    username:"samcyril",
+    password:"sam123"
+  },
+  {
+    username:"jaipal",
+    password:"pal123"
+  }
+]
+  
+
+let demo= document.getElementById("demo")
+function check(){
+let usern=document.getElementById("username").value
+let password=document.getElementById("password").value
+  console.log(usern)
+   let tempv= credentials.findIndex((element)=>element['username']===usern)
+   console.log(tempv)
+   
+if(tempv>-1){
+  
+  if(credentials[tempv]['username']==usern && credentials[tempv]['password']=== password)
+    {
+      document.getElementById('demo').innerHTML=`<p>logged in</p>`;
+    }
+    else
+    {
+      document.getElementById('demo').innerHTML=`<p>invalid password</p>`;
+
+    }
+}
+  else
+  {
+      document.getElementById('demo').innerHTML=`<p>Invalid username</p>`;
+
+  }
+//  demo.innerHTML=usern
+//   if( credentials.username===usern.value ){
+//     console.log('Logged In')
+//   }else{
+//     console.log("enter valid details")
+//   }
+}
